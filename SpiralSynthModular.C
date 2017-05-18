fd6f98a1a6f69542834fbbd342d2180a13ff8e5a
Julius Schmidt <aiju@phicode.de>
2017-05-18 13:23:08 +0100

    fix iterator bug

--- SpiralSynthModular.C
+++ SpiralSynthModular.C
@@ -244,7 +244,7 @@ void SynthModular::UpdatePluginGUIs()
 {
 	// see if any need deleting
 	for (map<int,DeviceWin*>::iterator i=m_DeviceWinMap.begin();
-		 i!=m_DeviceWinMap.end(); i++)
+		 i!=m_DeviceWinMap.end(); )
 	{
 		if (i->second->m_DeviceGUI && i->second->m_DeviceGUI->GetPluginWindow())
 		{
@@ -280,9 +280,12 @@ void SynthModular::UpdatePluginGUIs()
 			i->second->m_DeviceGUI = NULL;
 
 			//Erase from winmap if no audio to do it
-			if (erase)
-				m_DeviceWinMap.erase(i);
+			if (erase){
+				m_DeviceWinMap.erase(i++);
+				continue;
+			}
 		}
+		i++;
 	}
 
 	m_Canvas->Poll();
